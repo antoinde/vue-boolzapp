b2f6fdee2708187d109d8c1e356960523b48e2a2
antoinde <antonello.indelicato98@gmail.com>
2022-09-17 18:22:53 +0200

    Layout ultimato

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // Antonello's code. Git-Hub: antoinde
 
 console.log("JavaScript is working properly! =) Let's code!");
+
+// SVOLGIMENTO
+
+const app = new Vue ({
+    el: '#root',
+    data: contacts,
+    methods: {
+        getLastMsg(messagesArray){
+            // console.log(messagesArray.length);
+            let n= messagesArray.length - 1;
+            // console.log(n);
+            return n;
+        }
+    
+    }
+});
